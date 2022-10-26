2f446672ca4c28752190d574fc2ed479bbd5e384
Auvitly <nosfosters@mail.ru>
2022-10-26 10:47:14 +0300

    added todo list

---
+++ TODO.md
@@ -0,0 +1,2 @@
+1. If err -> zero value
+2. Full support types
--- config_test.go
+++ config_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"settings/internal"
 	"testing"
+	"time"
 )
 
 type Config struct {
@@ -15,12 +16,15 @@ type Subconfig struct {
 }
 
 type Simple struct {
-	//Int      int64         `json:"int" default:"10"`
-	//String   string        `json:"string" default:"hello"`
-	//Float64  float64       `json:"float64" default:"23.1"`
-	//Bool     bool          `json:"bool" default:"true"`
-	//Duration time.Duration `json:"dur" default:"1m"`
-	Servers []string `json:"servers" validation:"tcp_addr"`
+	Int      int64               `json:"int" default:"10"`
+	String   string              `json:"string" default:"hello"`
+	Float64  float64             `json:"float64" default:"23.1"`
+	Bool     bool                `json:"bool" default:"true"`
+	Duration time.Duration       `json:"dur" default:"1m"`
+	Servers  []*string           `json:"servers" validation:"tcp_addr"`
+	MapType1 map[string]string   `json:"map_type_1"`
+	MapType2 map[string][]string `json:"map_type_2"`
+	MapType3 map[string][]Simple `json:"map_type_3"`
 }
 
 func TestConfigurator(t *testing.T) {
